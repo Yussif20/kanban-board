5cb0fdfcbd760b84bf2daf7e2cff324c34b1bd6d
Yousef ayman <yousefayman2000500@gmail.com>
2024-10-16 03:34:08 +0300

    add the TextField component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Button from './components/Button/Button';
+import Button from './components/Button';
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="text-main-purple font-plus-jakarta text-heading-xl">
       <Button>Hello</Button>
+      <TextField />
     </div>
   );
 }
