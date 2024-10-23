46015a97f005dfe5e14cfb399277d527c5fd7f99
Yousef ayman <yousefayman2000500@gmail.com>
2024-10-23 09:41:31 +0300

    Dnd Logic pt.3

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -29,24 +29,29 @@ import {
  */
 
 export const WorkSpace = () => {
-  const { data, selectedBoardIndex, setData } = useContext(DataContext);
-
+  const { data, setData, selectedBoardIndex } = useContext(DataContext);
   const columns = data[selectedBoardIndex]?.columns;
 
-  const createNewColumn = (num) => {
-    return {
-      id: Date.now(),
-      title: `New Column ${num}`,
-      tasks: [],
-    };
-  };
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    })
+  );
+
+  const createNewColumn = (num) => ({
+    id: Date.now(),
+    title: `New Column ${num}`,
+    tasks: [],
+  });
 
   const addNewColumnHandler = () => {
-    const num = data[selectedBoardIndex].columns.length + 1;
+    const num = data[selectedBoardIndex].columns.length;
     const newColumn = createNewColumn(num);
 
-    setData((prevData) =>
-      produce(prevData, (draft) => {
+    setData((prev) =>
+      produce(prev, (draft) => {
         draft[selectedBoardIndex].columns.push(newColumn);
       })
     );
@@ -56,28 +61,18 @@ export const WorkSpace = () => {
     let tasksIds = [];
 
     if (!columns || columns.length === 0) return tasksIds;
-
     for (let column of columns) {
       tasksIds = [...tasksIds, ...column.tasks.map((task) => task.id)];
     }
     return tasksIds;
   }, [columns]);
 
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      activationConstraint: {
-        distance: 10,
-      },
-    })
-  );
   const onDragEndHandler = (event) => {
     const { active, over } = event;
-
     const activeId = active.id;
-    const activeColumnId = active.data.current.columnId;
     const overId = over.id;
     const overColumnId = over.data.current.columnId;
+    const activeColumnId = active.data.current.columnId;
 
     if (activeId === overId) return;
 
@@ -91,12 +86,53 @@ export const WorkSpace = () => {
             (task) => task.id === overId
           );
           const tasks = arrayMove(column.tasks, activeIdIndex, overIdIndex);
+
           return { ...column, tasks };
         }
         return column;
       });
-      setData((prevData) =>
-        produce(prevData, (draft) => {
+
+      setData((prev) =>
+        produce(prev, (draft) => {
+          draft[selectedBoardIndex].columns = newColumns;
+        })
+      );
+    }
+  };
+
+  const onDragOverHandler = (event) => {
+    const { active, over } = event;
+    const activeId = active.id;
+
+    const overColumnId = over?.data?.current?.columnId;
+    const activeColumnId = active?.data?.current?.columnId;
+
+    if (overColumnId && activeColumnId !== overColumnId) {
+      const newColumns = columns.map((column) => {
+        // if the column is the column that the card is dragged to then add the task to the column
+        if (column.id === overColumnId) {
+          // get the active task from the active column's tasks
+          const activeTask = columns
+            .find((column) => column.id === activeColumnId)
+            .tasks.find((task) => task.id === activeId);
+          // add the active task to the end of the new column's tasks because the dnd lib will handle the reordering
+          const tasks = [...column.tasks, activeTask];
+
+          return { ...column, tasks };
+        }
+
+        // if the column is the column that the card is dragged from then remove the task from the column
+        if (column.id === activeColumnId) {
+          const tasks = column.tasks.filter((task) => task.id !== activeId);
+
+          return { ...column, tasks };
+        }
+
+        return column;
+      });
+
+      setData((prev) =>
+        produce(prev, (draft) => {
           draft[selectedBoardIndex].columns = newColumns;
         })
       );
@@ -108,6 +144,7 @@ export const WorkSpace = () => {
       sensors={sensors}
       collisionDetection={closestCenter}
       onDragEnd={onDragEndHandler}
+      onDragOver={onDragOverHandler}
     >
       <div className="flex h-[calc(100vh-97px)] flex-1 gap-6 overflow-auto bg-light-grey p-6">
         <SortableContext
@@ -115,12 +152,12 @@ export const WorkSpace = () => {
           strategy={verticalListSortingStrategy}
         >
           {columns?.length &&
-            columns.map((column, index) => (
+            columns.map((item, index) => (
               <Column
-                id={column.id}
-                key={column.id}
-                title={column.title}
-                tasks={column.tasks}
+                key={`${item.id}${item.title}`}
+                id={item.id}
+                title={item.title}
+                tasks={item.tasks}
                 columnIndex={index}
               />
             ))}
